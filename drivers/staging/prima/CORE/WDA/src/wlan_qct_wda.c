f7f894ffe9499ec3c6cc6e9fa59afad325828c92
Dennis Rassmann <showp1984@gmail.com>
2012-12-18 05:12:45 +0100

    drivers/staging/prima/CORE/WDA/src/wlan_qct_wda: fix: 'warning: '...' may be used uninitialized in this function'
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	drivers/staging/prima/CORE/WDA/src/wlan_qct_wda.c

--- drivers/staging/prima/CORE/WDA/src/wlan_qct_wda.c
+++ drivers/staging/prima/CORE/WDA/src/wlan_qct_wda.c
@@ -1657,7 +1657,7 @@ uint8 WDA_IsWcnssWlanCompiledVersionGreaterThanOrEqual(uint8 major, uint8 minor,
 {    
     VOS_STATUS status = VOS_STATUS_SUCCESS;
     v_CONTEXT_t vosContext = vos_get_global_context(VOS_MODULE_ID_WDA, NULL);
-    tSirVersionType compiledVersion;
+    tSirVersionType compiledVersion = {revision = 0, version = 0, minor = 0, major = 0};
 
     status = WDA_GetWcnssWlanCompiledVersion(vosContext, &compiledVersion);
 
@@ -1679,7 +1679,7 @@ uint8 WDA_IsWcnssWlanReportedVersionGreaterThanOrEqual(uint8 major, uint8 minor,
 {    
     VOS_STATUS status = VOS_STATUS_SUCCESS;
     v_CONTEXT_t vosContext = vos_get_global_context(VOS_MODULE_ID_WDA, NULL);
-    tSirVersionType reportedVersion;
+    tSirVersionType reportedVersion = {revision = 0, version = 0, minor = 0, major = 0};
 
     status = WDA_GetWcnssWlanReportedVersion(vosContext, &reportedVersion);
 
