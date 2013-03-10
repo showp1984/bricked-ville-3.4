c8b1146f0d1f098364fc867146d42a2bfb8ec829
Dennis Rassmann <showp1984@gmail.com>
2013-03-10 07:31:56 +0100

    kernel/workqueue.c: Revert hacks for previous misuse of cpuinit
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	kernel/workqueue.c

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3596,7 +3596,7 @@ static int __devinit workqueue_cpu_callback(struct notifier_block *nfb,
  * Workqueues should be brought up before normal priority CPU notifiers.
  * This will be registered high priority CPU notifier.
  */
-static int __devinit workqueue_cpu_up_callback(struct notifier_block *nfb,
+static int workqueue_cpu_up_callback(struct notifier_block *nfb,
 					       unsigned long action,
 					       void *hcpu)
 {
@@ -3614,7 +3614,7 @@ static int __devinit workqueue_cpu_up_callback(struct notifier_block *nfb,
  * Workqueues should be brought down after normal priority CPU notifiers.
  * This will be registered as low priority CPU notifier.
  */
-static int __devinit workqueue_cpu_down_callback(struct notifier_block *nfb,
+static int workqueue_cpu_down_callback(struct notifier_block *nfb,
 						 unsigned long action,
 						 void *hcpu)
 {
