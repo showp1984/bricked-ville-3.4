faa70058f57740c43f880cfdc18e822cf041bf8d
Dennis Rassmann <showp1984@gmail.com>
2013-03-11 05:59:39 +0100

    arch/arm/mach-msm: Allow building without htc_util and fix building without it
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/include/mach/htc_util.h
+++ arch/arm/mach-msm/include/mach/htc_util.h
@@ -1,8 +1,28 @@
 #ifndef __ARCH_ARM_MACH_MSM_HTC_UTIL_H
 #define __ARCH_ARM_MACH_MSM_HTC_UTIL_H
+
+#ifdef CONFIG_HTC_UTIL
 void htc_pm_monitor_init(void);
 void htc_monitor_init(void);
 void htc_idle_stat_add(int sleep_mode, u32 time);
 void htc_xo_block_clks_count(void);
+#else
+static inline void htc_pm_monitor_init(void)
+{
+        return;
+}
+static inline void htc_monitor_init(void)
+{
+        return;
+}
+static inline void htc_idle_stat_add(int sleep_mode, u32 time)
+{
+        return;
+}
+static inline void htc_xo_block_clks_count(void)
+{
+        return;
+}
+#endif
 
 #endif
