6fbb42c4c5fff6a485f05b6293bd88dac6e8e728
Dennis Rassmann <showp1984@gmail.com>
2013-05-04 21:08:18 +0200

    acpu/board: lower min constraints further and fix max constraints for cpu uv/ov
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -1530,8 +1530,8 @@ static void __init bus_init(unsigned int init_bw)
 
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 
-#define HFPLL_MIN_VDD		 800000
-#define HFPLL_MAX_VDD		1350000
+#define HFPLL_MIN_VDD		 700000
+#define HFPLL_MAX_VDD		1425000
 
 ssize_t acpuclk_get_vdd_levels_str(char *buf) {
 
