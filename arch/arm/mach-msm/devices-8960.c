8819cb7b66ce105a1496067e425ac434e1f29bdb
Dennis Rassmann <showp1984@gmail.com>
2013-05-05 19:56:06 +0200

    Revert "gpu: increase bus bandwidth for high/max vectors"
    This reverts commit 7276d33074ffd13297b5efdf6d9124922ed6e5fe.
    
    Conflicts:
    	arch/arm/mach-msm/devices-8960.c

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -2905,7 +2905,7 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(4264),
+		.ib = KGSL_CONVERT_TO_MBPS(2656),
 	},
 };
 
@@ -2914,7 +2914,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(5290),
+		.ib = KGSL_CONVERT_TO_MBPS(3968),
 	},
 };
 
