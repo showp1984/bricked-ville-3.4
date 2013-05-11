08033c0f5d0553bdfc788624507f3565f1c8da44
Dennis Rassmann <showp1984@gmail.com>
2013-05-11 04:35:10 +0200

    cpuinit: get rid of __cpuinit, second regexp This commit is the result of:
    find -name "*.c" | xargs sed -i "s/ __cpuexit / /g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- arch/blackfin/mach-common/smp.c
+++ arch/blackfin/mach-common/smp.c
@@ -479,7 +479,7 @@ EXPORT_SYMBOL(resync_core_dcache);
 #endif
 
 #ifdef CONFIG_HOTPLUG_CPU
-int __cpuexit __cpu_disable(void)
+int __cpu_disable(void)
 {
 	unsigned int cpu = smp_processor_id();
 
@@ -492,7 +492,7 @@ int __cpuexit __cpu_disable(void)
 
 static DECLARE_COMPLETION(cpu_killed);
 
-int __cpuexit __cpu_die(unsigned int cpu)
+int __cpu_die(unsigned int cpu)
 {
 	return wait_for_completion_timeout(&cpu_killed, 5000);
 }
